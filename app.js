13e0b9e8f7d16cf79a4fceb3b8798deae9a85b0e
gappy198 <mygappy21@gmail.com>
2024-02-12 17:53:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const port = 8080
 const route = require('./route/myRoute')
+const admin = require('./route/admin')
 
 app.use(route)
 app.get('/', (req, res) => {
     res.send("<h1>Homepage | GapDev</h1>")
 })
+
+app.use("/admin", admin)
 
 app.listen(port, () => {
     console.log("start server at port", port);
